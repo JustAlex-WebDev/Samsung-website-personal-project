d973c61df719c87a5bfdea66744700eb2ead5293
Alexandar <100527128+JustNewbieDev@users.noreply.github.com>
2022-03-15 16:39:17 +0200

    Updates and fixes
    Updated the new fully responsive grid part and fixed some issues!

--- samsung js.js
+++ samsung js.js
@@ -1,4 +1,4 @@
-//making the slides
+//making the slides for the first slider-container ADVANCED WAY OF DOING IT
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -19,19 +19,17 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
 
-
   /*decreases the dot's opacity after not beign active*/
-  for (i = 0; i < dots.length; i++) {
+  /*for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
 
   for (i = 0; i < bms.length; i++) {
       bms[i].className = bms[i].className.replace(" active2", "");
-  }
+  }*/
 
   /*makes the slides appear*/
   slides[slideIndex-1].style.display = "block";
-  
 
   /*increases the opacity of the active dot to see where you are*/
   /*
@@ -44,7 +42,132 @@ function showSlides(n) {
 
 
 
+//making the slides for the second slider-container FIRST WAY OF DOING IT ADVANCED
+/*var slideIndex2 = 1;
+showSlides2(slideIndex2);
 
+function currentSlide2(n2) {
+  showSlides2(slideIndex2 = n2);
+}
+
+function showSlides2(n2) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot");
+  var bms2 = document.getElementsByClassName("bm2");
+  if (n2 > slides2.length) {slideIndex2 = 1}
+  if (n2 < 1) {slideIndex2 = slides2.length}
+
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";
+  }
+
+  slides2[slideIndex2-1].style.display = "block";
+
+  bms2[slideIndex2-1].className += " active2";
+}*/
+
+
+
+
+//making the slides for the second slider-container SECOND WAY OF DOING IT SIMPLER
+//slide1
+function myFunction1() {
+  var x1 = document.querySelector(".first");
+  var x2 = document.querySelector(".second");
+  var x3 = document.querySelector(".third");
+  var x4 = document.querySelector(".fourth");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+  } else {
+    x1.style.display = "block";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+  }
+}
+
+//slide2
+function myFunction2() {
+  var x1 = document.querySelector(".first");
+  var x2 = document.querySelector(".second");
+  var x3 = document.querySelector(".third");
+  var x4 = document.querySelector(".fourth");
+  if (x2.style.display === "none") {
+    x1.style.display = "none";
+    x2.style.display = "block";
+    x3.style.display = "none";
+    x4.style.display = "none";
+  } else {
+    x1.style.display = "none";
+    x2.style.display = "block";
+    x3.style.display = "none";
+    x4.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var x1 = document.querySelector(".first");
+  var x2 = document.querySelector(".second");
+  var x3 = document.querySelector(".third");
+  var x4 = document.querySelector(".fourth");
+  if (x3.style.display === "none") {
+    x1.style.display = "none";
+    x2.style.display = "none";
+    x3.style.display = "block";
+    x4.style.display = "none";
+  } else {
+    x1.style.display = "none";
+    x2.style.display = "none";
+    x3.style.display = "block";
+    x4.style.display = "none";
+  }
+}
+
+function myFunction4() {
+  var x1 = document.querySelector(".first");
+  var x2 = document.querySelector(".second");
+  var x3 = document.querySelector(".third");
+  var x4 = document.querySelector(".fourth");
+  if (x3.style.display === "none") {
+    x1.style.display = "none";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "block";
+  } else {
+    x1.style.display = "none";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "block";
+  }
+}
+
+
+
+
+
+
+
+
+
+/*making the search bar appear and disappear easy*/
+function myFunction() {
+  var x = document.getElementById("mySearchBar");
+  var y = document.querySelector(".opacity");
+  var z = document.querySelector(".gg-close");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "flex";
+    y.style.display = "flex";
+    z.style.display = "flex";
+  }
+}
 
 
 
@@ -102,54 +225,5 @@ function showSuggestion(list){
 	}
 	suggBox.innerHTML = listData;
 }
-
-
-
-
-
-
-
-
-
-/*making the search bar appear and disappear easy*/
-function myFunction() {
-  var x = document.getElementById("mySearchBar");
-  var y = document.querySelector(".opacity");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-    y.style.display = "none";
-  } else {
-    x.style.display = "flex";
-    y.style.display = "flex";
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
